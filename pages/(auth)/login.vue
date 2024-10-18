7473e015f630abee6124698477e8e9199808ce8f
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-10-18 19:09:59 +0200

    feat. add login page

---
+++ pages/(auth)/login.vue
@@ -0,0 +1,37 @@
+<script setup lang="ts">
+definePageMeta({
+  middleware: ["guest-only"],
+  layout: false,
+});
+
+const { login } = useAuth();
+
+let form = reactive({
+  username: '',
+  password: '',
+  remerber: false
+})
+
+let connect = async () => {
+  try {
+
+    await login(form.email, form.password, form.remember);
+
+  } catch {
+
+  }
+}
+</script>
+
+<template>
+  <form @submit.prevent="connect">
+    <h1>Login</h1>
+    <input type="text" placeholder="Username" v-model="loginForm.username" />
+    <input type="password" placeholder="Password" v-model="loginForm.password" />
+    <button type="submit">Login</button>
+  </form>
+</template>
+
+<style scoped>
+
+</style>
