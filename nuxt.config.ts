ed8ccb0e695c81b5f186376f9f99daca97238346
C.Nelhomme <64921289+D3ller@users.noreply.github.com>
2024-10-18 19:15:04 +0200

    fix. cookie in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+const ONE_DAY = 60 * 60 * 24 * 1000;
+const ONE_WEEK = ONE_DAY * 7;
+
 export default defineNuxtConfig({
   devtools: { enabled: true },
   modules: [
@@ -10,6 +13,8 @@ export default defineNuxtConfig({
 
   runtimeConfig: {
     cookieName: "session",
-    cookieSecret: process.env.COOKIE_SECRET
+    cookieSecret: process.env.COOKIE_SECRET,
+    cookieExpires: ONE_DAY.toString(),
+    cookieRememberMeExpires: ONE_WEEK.toString(),
   },
 })
